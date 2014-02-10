9aff24c07c6976c2a0672a15b27ae81c97ae317d
Joe Cross <joe.mcross@gmail.com>
2014-02-09 21:53:43 -0800

    Add Graphics.pixel() for white pixel, tint helpers

--- EngineCore/src/crossj/engine/util/Graphics.java
+++ EngineCore/src/crossj/engine/util/Graphics.java
@@ -44,6 +44,13 @@ public class Graphics {
         return outVec;
     }
 
+    /**
+     * Return white pixel
+     */
+    public static Texture pixel() {
+        return pixel(1, 1, 1, 1);
+    }
+
     public static Texture pixel(float r, float g, float b) {
         return pixel(r, g, b, 1);
     }
@@ -63,11 +70,49 @@ public class Graphics {
      * Convenience for rendering an entire texture with a given position,
      * origin, dimensions, and rotation.
      *
-     * Rotation is provided in radians.
+     * Rotation should be in radians.
      */
     public static void draw(SpriteBatch spriteBatch, Texture texture, Vector2 position, Vector2 origin,
             Vector2 dimensions, float rotation) {
         spriteBatch.draw(texture, position.x, position.y, origin.x, origin.y, dimensions.x, dimensions.y, 1, 1,
                 rotation * MathUtils.radiansToDegrees, 0, 0, texture.getWidth(), texture.getHeight(), false, false);
+    }
+
+    static final Color oldTint = new Color();
+    static boolean isTinting = false;
+
+    /**
+     * Set the tint for a sprite batch, preserving the tint. Revert the tint
+     * color with {@link #stopTint(SpriteBatch)}.
+     *
+     * Not thread safe, do not nest start calls, even for different sprite
+     * batches.
+     *
+     * @param spriteBatch
+     * @param tint
+     */
+    public static void startTint(SpriteBatch spriteBatch, Color tint) {
+        if (isTinting) {
+            throw new RuntimeException("Tried to start tinting again without calling stopTint first!");
+        }
+        isTinting = true;
+        oldTint.set(spriteBatch.getColor());
+        spriteBatch.setColor(tint);
+    }
+
+    /**
+     * Revert the tint for a sprite batch.
+     *
+     * Not thread safe, do not nest stop calls, even for different sprite
+     * batches.
+     *
+     * @param spriteBatch
+     */
+    public static void stopTint(SpriteBatch spriteBatch) {
+        if (!isTinting) {
+            throw new RuntimeException("Tried to stop tinting without calling startTine first!");
+        }
+        isTinting = false;
+        spriteBatch.setColor(oldTint);
     }
 }
