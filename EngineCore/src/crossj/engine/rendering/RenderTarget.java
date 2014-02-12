52df1f929159c3f10caca881d0d9a0268bb20a89
Joe Cross <joe.mcross@gmail.com>
2014-02-11 17:54:29 -0800

    Rename FBORenderTarget -> RenderTarget

--- EngineCore/src/crossj/engine/rendering/RenderTarget.java
+++ EngineCore/src/crossj/engine/rendering/RenderTarget.java
@@ -1,79 +1,18 @@
 package crossj.engine.rendering;
 
-import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.graphics.Pixmap.Format;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
-import com.badlogic.gdx.graphics.glutils.FrameBuffer;
-import com.badlogic.gdx.math.MathUtils;
-import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.utils.Disposable;
 
-public class RenderTarget {
-    private final TextureRegion fboRegion;
-    private final FrameBuffer fbo;
+public interface RenderTarget extends Disposable {
 
-    public RenderTarget(Vector2 dimensions) {
-        this((int) dimensions.x, (int) dimensions.y);
-    }
+    public void begin();
 
-    public RenderTarget(int width, int height) {
-        // from https://github.com/mattdesl/lwjgl-basics/wiki/LibGDX-Textures
-        boolean npotSupported = Gdx.graphics.supportsExtension("GL_OES_texture_npot")
-                || Gdx.graphics.supportsExtension("GL_ARB_texture_non_power_of_two");
-        if (npotSupported) {
-            fbo = new FrameBuffer(Format.RGBA8888, width, height, true);
-            fboRegion = new TextureRegion(fbo.getColorBufferTexture());
-        } else {
-            int texWidth = MathUtils.nextPowerOfTwo(width);
-            int texHeight = MathUtils.nextPowerOfTwo(height);
-            fbo = new FrameBuffer(Format.RGBA8888, texWidth, texHeight, true);
-            fboRegion = new TextureRegion(fbo.getColorBufferTexture(), 0, texHeight - height, width, height);
-        }
-        fboRegion.flip(false, true);
-    }
+    public void end();
 
-    public void begin() {
-        fbo.begin();
-    }
+    public TextureRegion getRegion();
 
-    public void end() {
-        fbo.end();
-    }
+    public int getWidth();
 
-    public TextureRegion getRegion() {
-        return fboRegion;
-    }
+    public int getHeight();
 
-    public int getWidth() {
-        return fboRegion.getRegionWidth();
-    }
-
-    public int getHeight() {
-        return fboRegion.getRegionHeight();
-    }
-
-    /**
-     * Render one target onto another.  Useful for processing pipelines.
-     *
-     * @param src
-     * @param dst
-     * @param spriteBatch
-     * @param shader
-     */
-    public static void render(RenderTarget src, RenderTarget dst, SpriteBatch spriteBatch) {
-        dst.begin();
-        spriteBatch.begin();
-        spriteBatch.draw(src.getRegion(), 0, 0);
-        spriteBatch.end();
-        dst.end();
-    }
-
-    /**
-     * Post-processing pipeline, render each target onto the next
-     */
-    public static void render(SpriteBatch batch, RenderTarget... targets) {
-        for (int i = 0; i < targets.length - 1; i++) {
-            render(targets[i], targets[i + 1], batch);
-        }
-    }
 }
